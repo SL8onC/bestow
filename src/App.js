b9f213efc875f77f2b1cfb9e70a8425d995dc923
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2021-01-23 15:50:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import logo from './giftpictures/001-gift.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+import { Transition } from 'react-spring/renderprops';
 
-export default App;
+export default class App extends Component {
+	constructor() {
+		super();
+
+		this.state = { beginFlip: false, items: [1, 2, 3, 4] };
+		this.onClick = this.onClick.bind(this);
+	}
+	onClick() {
+		this.setState({ beginFlip: !this.state.beginFlip });
+	}
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					{/* prettier-ignore */}
+					<pre>B E S T <img src={logo} className='App-logo' alt='logo' onClick={this.onClick}/> W</pre>
+					<p>a minimalist gift registry</p>
+				</header>
+
+				<Transition
+					items={this.state.items}
+					// keys={(item) => item.key}
+					from={{ transform: 'translate3d(0,-40px,0)' }}
+					enter={{ transform: 'translate3d(0,0px,0)' }}
+					leave={{ transform: 'translate3d(0,-40px,0)' }}
+				>
+					{(item) => (props) => <div style={props}>{item.text}</div>}
+				</Transition>
+			</div>
+		);
+	}
+}
