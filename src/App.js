1c304301db4869d1d6f82dfbb85a48cac0ccaa97
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2021-01-24 21:16:40 -0500

    refactor (App.js is simplified, styling updated)

--- src/App.js
+++ src/App.js
@@ -4,21 +4,10 @@ import { LandingPage } from './components/';
 import React, { Component } from 'react';
 
 export default class App extends Component {
-	state = {
-		wedding: false,
-		party: false,
-		baby: false,
-		toggleChoice: false
-	};
-
-	onClick = () => {
-		this.setState({ toggleChoices: true });
-		console.log('click state-->', this.state);
-	};
 	render() {
 		return (
 			<div>
-				<LandingPage onClick={this.onClick} />
+				<LandingPage />
 			</div>
 		);
 	}
